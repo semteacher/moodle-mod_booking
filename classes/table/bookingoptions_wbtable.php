2f9fcffcecad5e905f2c2030b0d1a24d17baf48c
Andrii Semenets <semteacher@gmail.com>
2026-05-25 12:05:13 +0300

    fix 3 accessibility violations "Element does not have inner text that is visible to screen readers" for the #action-menu-toggle-183002 (https://github.com/Wunderbyte-GmbH/Wunderbyte-GmbH/issues/1597)

--- classes/table/bookingoptions_wbtable.php
+++ classes/table/bookingoptions_wbtable.php
@@ -1414,16 +1414,16 @@ class bookingoptions_wbtable extends wunderbyte_table {
                     <button class="bookingoption-edit-button dropdown-toggle btn btn-light btn-sm text-primary ms-1"
                         id="action-menu-toggle-' .
                         $optionid .
-                        '" title="" role="button" data-toggle="dropdown" data-bs-toggle="dropdown"
-                        aria-haspopup="true" aria-expanded="false">
+                        '" role="button" data-toggle="dropdown" data-bs-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false" aria-label="' . get_string('settings') . '">
                         <i class="icon fa fa-cog me-0" aria-hidden="true"
-                            aria-label="' . get_string('settings') . '" title="' . get_string('settings') . '" >
-                        </i>
+                            aria-label="' . get_string('settings') . '" title="' . get_string('settings') . '"></i>
+                        <span class="visually-hidden">' . get_string('settings') . '</span>
                     </button>
                     <div class="dropdown-menu dropdown-menu-right dropdown-menu-end menu align-tr-br" id="action-menu-' .
                 $optionid .
                 '-menu" data-rel="menu-content"
-                        aria-labelledby="action-menu-toggle-3" role="menu" data-align="tr-br">';
+                        aria-labelledby="action-menu-toggle-' . $optionid . '" role="menu" data-align="tr-br">';
             $ret .= implode($ddoptions);
             $ret .= '</div></div>';
         }
